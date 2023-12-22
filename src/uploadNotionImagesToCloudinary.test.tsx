66630dabf192b001dd5db6dcc67b40c210849b61
Al <al@pachama.com>
2023-12-22 12:29:16 -0800

    feat: add test and ci

---
+++ src/uploadNotionImagesToCloudinary.test.tsx
@@ -0,0 +1,72 @@
+import uploadNotionImagesToCloudinary from "./uploadNotionImagesToCloudinary";
+
+jest.mock("./lib/cloudinaryClient", () => ({
+  config: jest.fn(),
+  uploadImage: jest.fn().mockImplementation(() => ({ url: "" })),
+}));
+
+jest.mock("./lib/notionClient", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      getPage: async () => ({
+        id: "fake-notion-page-id",
+        cover: {
+          type: "file",
+          file: {
+            url: "fake-page-url",
+          },
+        },
+      }),
+      updatePageCoverExternalUrl: async () => {},
+      fetchAllImageBlocks: async () => {
+        return [
+          // an image already hosted in notion
+          {
+            image: {
+              type: "file",
+              file: {
+                url: "fake image url",
+              },
+            },
+          },
+          // another image hosted in of notion
+          {
+            image: {
+              type: "file",
+              file: {
+                url: "fake image url",
+              },
+            },
+          },
+          // an image hosted outside of notion
+          {
+            image: {
+              type: "",
+            },
+          },
+        ];
+      },
+      updateImageBlockExternalUrl: async () => {},
+    };
+  });
+});
+
+jest.mock("./utils/downloadFile", () => jest.fn());
+jest.mock("./utils/makeFilenameFromCaption", () => jest.fn());
+
+describe("uploadNotionImagesToCloudinary", () => {
+  it("should upload an image to cloudinary for each image that is still hosted in notion", async () => {
+    const { uploadImage } = require("./lib/cloudinaryClient");
+
+    // @ts-expect-error - type narrowing isn't correct
+    await uploadNotionImagesToCloudinary({
+      cloudinaryUrl: "",
+      notionToken: "fake-notion-token",
+      notionDatabaseId: "fake-notion-db-id",
+      notionPageId: "fake-notion-page-id",
+    });
+
+    // 1 (cover image) + 2 (still hosted in notion) = 2
+    expect(uploadImage).toHaveBeenCalledTimes(3);
+  });
+});
